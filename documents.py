cf0d4860418ca5fc116d982b33dbaf9059e725cc
turbosheep44 <matthew.axisa4@gmail.com>
2020-01-10 17:07:16 +0100

    parse metadata and output graph statistics

--- documents.py
+++ documents.py
@@ -2,13 +2,14 @@ from itertools import combinations
 from json import dumps as json_dumps
 from json import load as load_json
 from collections import deque
+from ast import literal_eval
 
 
 # list intersect
 # given two lists, finds the common elements in O(n) time
 def intersect(listA, listB):
     setA = set(listA)
-    return [x for x in listB if x in setA]
+    return list(setA.intersection(listB))
 
 
 # read the metadata json file
@@ -16,44 +17,58 @@ with open('metadata.json') as json_file:
     data = load_json(json_file)
 
 # remove all those whose weight is not in the top 150
-users_to_keep = []
-for user in data.keys():
-    position = 0
-    while position < len(users_to_keep) and data[users_to_keep[position]]["weight"] < data[user]["weight"]:
-        position += 1
-    users_to_keep.insert(position, user)
-    users_to_keep = users_to_keep[-150:]
+# users_to_keep = []
+# for user in data.keys():
+#     position = 0
+#     while position < len(users_to_keep) and data[users_to_keep[position]]["weight"] < data[user]["weight"]:
+#         position += 1
+#     users_to_keep.insert(position, user)
+#     users_to_keep = users_to_keep[-100:]
 
-filtered_data = {user: data[user] for user in users_to_keep}
-for x in filtered_data:
-    print(x, filtered_data[x]["weight"])
+# filtered_data = {user: data[user] for user in users_to_keep}
+# for x in filtered_data:
+#    print(x, filtered_data[x]["weight"])
+
 # get a list of all the distinct user pairs
-distinct_user_pairs = combinations(list(filtered_data.keys()), 2)
-total_user_pairs = sum(1 for _ in combinations(list(filtered_data.keys()), 2))
+distinct_user_pairs = combinations(list(data.keys()), 2)
+total_user_pairs = sum(1 for _ in combinations(list(data.keys()), 2))
 
-# record the id of each user
+# record a numberical id for each user
 user_ids = {name: index for index,
-            name in enumerate(list(filtered_data.keys()))}
+            name in enumerate(list(data.keys()))}
 
 # for each distinct user pair
 #   one = userA.send intersect userB.received
 #   two = userB.send intersect userA.received
-#   document['AB'] = one union two
+#   document['AB'] = { one } union { two }
 documents = {}
 counter = 0
 print(f"Total users pairs: {total_user_pairs} =>       ",  end="")
+
 for user_pair in distinct_user_pairs:
+    # counter output
     counter += 1
     print('\b\b\b\b\b\b', end="", flush=True)
     print("{:6}".format(counter), end="")
+
+    # find common emails in sent and received and union the two
     common_emails = \
         intersect(data[user_pair[0]]["sent"], data[user_pair[1]]["received"]) + \
         intersect(data[user_pair[1]]["sent"], data[user_pair[0]]["received"])
-    if len(common_emails) > 1:
-        documents[str(user_ids[user_pair[0]]) + "|" +
-                  str(user_ids[user_pair[1]])] = common_emails
 
-print("\noutput document list")
-with open("document_list.json", "w") as output:
-    print(json_dumps({"users": user_ids,
-                      "documents": documents}, indent=4), file=output)
+    # if the edge actually has any weight (i.e. there exist common emails) then save it
+    if len(common_emails) > 1:
+        documents["(" + str(user_ids[user_pair[0]]) + ", " +
+                  str(user_ids[user_pair[1]]) + ")"] = common_emails
+
+print("\noutputting dtaa")
+with open("edges.json", "w") as output:
+    print(json_dumps(documents), file=output)
+
+with open("graph.json", "w") as output:
+    print(json_dumps({
+        "users": [{"id": user_ids[user], "name": user, "contact": data[user]["weight"]}
+                  for user in data],
+        "edges":  [{"source": literal_eval(edge)[0], "target": literal_eval(
+            edge)[1], "weight": len(documents[edge])} for edge in documents]
+    }), file=output)
---
+++ graph_faxxx.py
@@ -0,0 +1,181 @@
+from json import load as load_json
+from json import dumps as json_dumps
+from ast import literal_eval
+from math import inf
+
+
+# using floyd warshall's algorithm to get the shortest path between all nodes
+def floyd_warshall(node_count, adjacency_list):
+    # create a 2d array for all path distances (all distances start at infinity)
+    distance_matrix = \
+        [[inf for _ in range(node_count)] for _ in range(node_count)]
+
+    # for all the edges in the graph, set the distance matrix value to 1
+    # (use 1 since edges have no cost, all that matters is the number of edges)
+    for x in adjacency_list:
+        for y in adjacency_list[x]:
+            distance_matrix[x][y] = 1
+
+    # distance from any node to itself is 0
+    for x in range(node_count):
+        distance_matrix[x][x] = 0
+
+    # now use floyd warshall
+    for k in range(node_count):
+        for i in range(node_count):
+            for j in range(node_count):
+                if distance_matrix[i][j] > (distance_matrix[i][k] + distance_matrix[k][j]):
+                    distance_matrix[i][j] = distance_matrix[i][k] + \
+                        distance_matrix[k][j]
+
+    # ensure that all nodes are connected (no paths have infinite weight)
+    # and set the node_data value for eccentricity and average path distance
+    for node in range(node_count):
+        eccentricity = max(distance_matrix[node])
+        if eccentricity == inf:
+            print(f"[{node}] is not fully connected [{distance_matrix[node]}]")
+            print("FAIL! GRAPH IS NOT CONNECTED!")
+        node_data[node]["eccentricity"] = eccentricity
+        node_data[node]["average_path_distance"] = sum(
+            distance_matrix[node]) / len(distance_matrix[node])
+
+        # extract the diameter (max eccentricity)
+    diameter = max(max(distance_matrix[node]) for node in range(node_count))
+    print(f"Diameter [{diameter}]")
+
+    # extract average path length
+    average_path_length = sum(
+        sum(distance_matrix[node]) / (node_count-1) for node in range(node_count))/node_count
+    print(f"Average path length [{average_path_length}]")
+
+    # show paths which have maximum length
+    # counter = 0
+    # for x in range(node_count):
+    #     for y in range(node_count):
+    #         if distance_matrix[x][y] == diameter:
+    #             print(f"from [{x}] to [{y}]")
+    #             counter += 1
+    # print(f"# of people [{counter/2}]")
+
+
+def local_clustering_cofficient(node_index, adjacency_list):
+    # get the neighbour node list
+    neighbours = set(adjacency_list[node_index].keys())
+    node_data[node_index]["neighbours"] = len(neighbours)
+
+    # if the degree of this vertex is not > 1 then the clustering coefficient is undefined
+    if not len(neighbours) > 1:
+        # print(f"Local Clustering Coefficient of [{node_index}] is [-1]")
+        node_data[node_index]["clustering_coefficient"] = -1
+        return -1
+
+    # calculate the number of potential edges in the node induced subgraph of the neighbours
+    max_possible_neighbour_edges = (len(neighbours) * (len(neighbours) - 1))/2
+
+    # calculate the actual number of edges between neighbours
+    neighbour_edges = 0
+    for neighbour in neighbours:
+        for other_neighbour in neighbours:
+            if other_neighbour in adjacency_list[neighbour]:
+                neighbour_edges += 1
+    # divide by 2 (dont count duplicates)
+    neighbour_edges /= 2
+
+    # print(f"Neighbours {neighbours}")
+    # print(f"Max edges between neighbours [{max_possible_neighbour_edges}]")
+    # print(f"Actual between neighbours [{neighbour_edges}]")
+    # print(
+    #    f"Local Clustering Coefficient of [{node_index}] is [{neighbour_edges/max_possible_neighbour_edges}]")
+
+    node_data[node_index]["clustering_coefficient"] = neighbour_edges / \
+        max_possible_neighbour_edges
+    return node_data[node_index]["clustering_coefficient"]
+
+
+# removes the undefined (-1) coefficients from the list
+def filter_undefined_coefficients(list_of_coefficients):
+    for coefficient in list_of_coefficients:
+        if coefficient != -1:
+            yield coefficient
+
+
+# calculate the clustering coefficient for the whole graph
+def global_clustering_coefficient(node_count, adjacency_list):
+    # calculate the clustering coefficient for each node
+    local_coefficients = [local_clustering_cofficient(
+        node_index, adjacency_list) for node_index in range(node_count)]
+
+    # sum and count the total coefficients excluding those which are undefined (-1)
+    # the filter function allows for a in iterator which yields values,
+    # meaning that both operations can be done in one pass or memory instead of two
+    total_coefficient_value = 0
+    valid_coefficient_count = 0
+    for valid_coefficient in filter_undefined_coefficients(local_coefficients):
+        valid_coefficient_count += 1
+        total_coefficient_value += valid_coefficient
+
+    # calculate and return global clustering coefficient
+    # print(
+    #     f"Total Coefficient [{total_coefficient_value}]")
+    # print(
+    #     f"Number of valid coefficients [{valid_coefficient_count}]")
+    print(
+        f"Global clustering coefficient [{total_coefficient_value / valid_coefficient_count}]")
+    return total_coefficient_value / valid_coefficient_count
+
+
+# creates an adjacancy list for the given graph
+def construct_adjacency_list(edges):
+    # make an adjacency matrix of the correct size
+    # (using dictionaries for constant lookup time)
+    adjacency_list = {}
+    for edge in edges:
+        adjacency_list[edge[0]] = adjacency_list.get(edge[0], {})
+        adjacency_list[edge[0]][edge[1]] = edges[edge]
+        adjacency_list[edge[1]] = adjacency_list.get(edge[1], {})
+        adjacency_list[edge[1]][edge[0]] = edges[edge]
+
+    return adjacency_list
+
+
+# put relevant node data from the graph json into node_data
+def get_node_data_from_graph(graph):
+    for node in graph["users"]:
+        node_data[node["id"]]["name"] = node["name"]
+        node_data[node["id"]]["weight"] = node["contact"]
+
+
+if __name__ == "__main__":
+    # read the metadata json file
+    with open('graph.json') as json_file:
+        graph = load_json(json_file)
+
+    # perform conversions to edges = { tuple(id, id) : weight } and get node_count
+    edges = {(edge["source"],  edge["target"]): edge["weight"]
+             for edge in graph["edges"]}
+    node_count = len(graph["users"])
+
+    # graph size
+    print(f"Nodes  [{node_count}]")
+    print(f"Edges  [{len(edges)}]")
+
+    # create node_data dictionary and initialise the container for each node
+    global node_data
+    node_data = {index: {} for index in range(node_count)}
+
+    # extract useful node data from graph
+    get_node_data_from_graph(graph)
+
+    # create adjacency list and release memory which is no longer required
+    adjacency_list = construct_adjacency_list(edges)
+    del edges
+    del graph
+
+    # get fgraph facts
+    floyd_warshall(node_count, adjacency_list)
+    global_clustering_coefficient(node_count, adjacency_list)
+
+    # output the node data to files
+    for node_id in node_data:
+        with open(f"node_data/{node_id}.json", "w") as outputFile:
+            print(json_dumps(node_data[node_id]), file=outputFile)
